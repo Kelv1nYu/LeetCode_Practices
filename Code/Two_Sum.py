8b96e02ab8b133e0edf7cb432a51e14de922397a
Kelv1nYu <ykq1312@126.com>
2020-08-05 18:06:16 -0400

    new review and code

--- Code/Two_Sum.py
+++ Code/Two_Sum.py
@@ -9,11 +9,15 @@ class Solution:
         # Create dictionary for storing result
         rest = dict()
 
-
+        # Traverse the list
         for index in range(len(nums)):
+
+            # Check if the value was stored in the dictionary as the key
             if nums[index] not in rest.keys():
+                # If False, use difference between the target and the value as the key, and the current index will be stored in the dictionary as the value
                 rest[target - nums[index]] = index
             else:
+                # If True, return difference index and current index
                 return [rest[nums[index]], index]
 
         return None
