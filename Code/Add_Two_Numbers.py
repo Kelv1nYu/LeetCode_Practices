bc606845d4b77da2cac8374bd6ec2984ea0195e3
Kelv1nYu <ykq1312@126.com>
2021-03-17 20:05:19 -0400

    Add Two Numbers

--- Code/Add_Two_Numbers.py
+++ Code/Add_Two_Numbers.py
@@ -1,11 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
@@ -13,24 +13,27 @@ class Solution:
         """
 
         # Create a new ListNode as head of returned linked list
-        new_list_start = ListNode(0)
-        curr = new_list_start
-        carry = 0
+        head = ListNode(0)
+
+        n1, n2, carry, curr = 0, 0, 0, head
         
         while l1 or l2 or carry:
             
             # Check whether the current node is None, if true, assign it to the variable, otherwise the variable value is 0
-            v1 = l1.val if l1 else 0
-            v2 = l2.val if l2 else 0
+            n1 = l1.val if l1 else 0
+            n2 = l2.val if l2 else 0
             
             # The divmod() method takes two numbers and returns a pair of numbers (a tuple) consisting of their quotient and remainder.
-            carry, out = divmod(v1 + v2 + carry, 10)
-            
+            carry, out = divmod(n1 + n2 + carry, 10)
             curr.next = ListNode(out)
             curr = curr.next
+            # Or can just use // and %
+            # curr.next = ListNode((n1 + n2 + carry)%10)
+            # curr = curr.next
+            # carry = (n1 + n2 + carry) // 10
             
             l1 = l1.next if l1 is not None else None
             l2 = l2.next if l2 is not None else None
         
         
-        return new_list_start.next
+        return head.next
